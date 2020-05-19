6025a18aff3a6bbb975c54ab475ec846a411743d
arash.sr7 <arash.sr7@gmail.com>
2020-05-19 21:47:53 +0430

    dependencyContatiner??

---
+++ microRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Micro.Rabbit.Infra.Bus;
+using MicroRabbit.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
